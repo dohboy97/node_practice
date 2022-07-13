9760925f87fee54e00c52f137d4a03ecc97f7679
Connor <connor.doherty97@gmail.com>
2022-07-13 14:58:38 -0400

    test before running on local port

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express
+const PORT = 8000
+
+const blueChipAPI = {
+
+    'Microsoft':{
+        'Ticker':'MSFT',
+        'Value': 252.3,
+        'Market Cap': '1.89 Trillion'
+    },
+    'Apple':{
+        'Ticker':'AAPL',
+        'Value':146.33,
+        'Market Cap': '2.37 Trillion'
+    },
+    'Amazon':{
+        'Ticker':'AMZN',
+        'Value':111.14,
+        'Market Cap': '1.89 Trillion'
+    }
+
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT,()=>{
+    console.log('Node running')
+})
