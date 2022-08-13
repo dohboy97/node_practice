0d802631ccb6d8bf984092d96e05a7aaa0d52bfd
Connor <connor.doherty97@gmail.com>
2022-08-13 13:12:04 -0400

    ejs packages installed

--- server.js
+++ server.js
@@ -14,40 +14,48 @@ MongoClient.connect(dbConnectionStr, {useUnifiedTopology:true})
 .then(client => {
     console.log('Connected to the Blue Chip API database.')
     const db = client.db('blue_chip_api')
+    const blueChipCollection = db.collection(blue-chips)
+
+    app.get('/', (req, res)=>{
+        db.collection('blue-chips').find().toArray()
+        .then(results=>{
+            res.render('index.ejs',{ stocks : results})
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    })
+    
+    app.get('/api/:blueChipName',(request,response)=>{
+        const blueChipName = request.params.blueChipName.toLowerCase()
+        if(blueChipAPI[blueChipName]){
+        response.json(blueChipAPI[blueChipName])
+        }else{
+            response.json(blueChipAPI['unknown'])
+        }
+    })
+    
+    app.delete('/api/:blueChipName',(request,response)=>{
+        const blueChipName = request.params.blueChipName.toLowerCase()
+        delete blueChipAPI[blueChipName]
+    
+        response.status(204).end()
+    })
+    
+    
+    app.post('/api/blueChipAPI',(request, response)=>{
+        const blueChipName = request.body
+        console.log(blueChipName)
+        response.json(blueChipName)
+    })
+    
+    
+    
+    app.listen(process.env.PORT||PORT,()=>{
+        console.log('Node running on Heroku')
+    })
 })
 
 
 
 
-app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/index.html')
-})
-
-app.get('/api/:blueChipName',(request,response)=>{
-    const blueChipName = request.params.blueChipName.toLowerCase()
-    if(blueChipAPI[blueChipName]){
-    response.json(blueChipAPI[blueChipName])
-    }else{
-        response.json(blueChipAPI['unknown'])
-    }
-})
-
-app.delete('/api/:blueChipName',(request,response)=>{
-    const blueChipName = request.params.blueChipName.toLowerCase()
-    delete blueChipAPI[blueChipName]
-
-    response.status(204).end()
-})
-
-
-app.post('/api/blueChipAPI',(request, response)=>{
-    const blueChipName = request.body
-    console.log(blueChipName)
-    response.json(blueChipName)
-})
-
-
-
-app.listen(process.env.PORT||PORT,()=>{
-    console.log('Node running on Heroku')
-})
